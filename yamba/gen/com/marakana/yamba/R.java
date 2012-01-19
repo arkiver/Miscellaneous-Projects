f70290fbddbadc118bdb8630e452280121628e9f
Akshay Khole <akshay2009ark@gmail.com>
2012-01-20 02:04:49 +0530

    Completed update routing and implemented Toast pop ups in the screen

--- yamba/gen/com/marakana/yamba/R.java
+++ yamba/gen/com/marakana/yamba/R.java
@@ -22,9 +22,11 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040001;
-        public static final int buttonUpdate=0x7f040005;
+        public static final int buttonUpdate=0x7f040007;
         public static final int hello=0x7f040000;
         public static final int hintStatus=0x7f040004;
+        public static final int msgStatusUpdateFailed=0x7f040005;
+        public static final int msgStatusUpdatedSuccessfully=0x7f040006;
         public static final int statusUpdate=0x7f040002;
         public static final int titleStatusUpdate=0x7f040003;
     }
--- yamba/src/com/marakana/yamba/StatusActivity.java
+++ yamba/src/com/marakana/yamba/StatusActivity.java
@@ -1,5 +1,7 @@
 package com.marakana.yamba;
 
+import winterwell.jtwitter.Twitter;
+import winterwell.jtwitter.TwitterException;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
@@ -7,32 +9,46 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class StatusActivity extends Activity implements OnClickListener {
-	
+
 	EditText editStatus;
 	Button buttonUpdate;
-	
-	
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.status);
-        
-    editStatus = (EditText) findViewById(R.id.editStatus);
-    buttonUpdate = (Button) findViewById(R.id.buttonUpdate);
-    
-    buttonUpdate.setOnClickListener(this);
-        
-    }
 
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.status);
+
+		editStatus = (EditText) findViewById(R.id.editStatus);
+		buttonUpdate = (Button) findViewById(R.id.buttonUpdate);
+
+		buttonUpdate.setOnClickListener(this);
+
+	}
 
 	@Override
 	public void onClick(View v) {
 		// TODO Auto-generated method stub
-	String status = editStatus.getText().toString();
-	Log.d("StatusActivity","Your recent status was: "+status);
-		
+		String status = editStatus.getText().toString();
+
+		try {
+			Twitter twitter = new Twitter("learningandroid", "pass2010");
+			twitter.setAPIRootUrl("http://learningandroid.status.net/api");
+			twitter.setStatus(status);
+			Toast.makeText(this, R.string.msgStatusUpdatedSuccessfully,
+					Toast.LENGTH_LONG).show();
+
+		} catch (TwitterException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			Toast.makeText(this, R.string.msgStatusUpdateFailed,
+					Toast.LENGTH_LONG).show();
+		}
+
+		Log.d("StatusActivity", "Your recent status was: " + status);
+
 	}
 }
